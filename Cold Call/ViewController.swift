7dadf85cffd6f4b19d55af89c0109424448f218f
Latifa881 <l-n-a-m@hotmail.com>
2021-12-02 02:09:11 -0800

    my first commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let names:[String]=["Latifa","Sara","Israa","Ikram","Ragahad","Jumanah","Asrar","Lana","Munazzah","Malak"]
+   
+    
+    @IBAction func coldCallButton(_ sender: Any) {
+       
+        nameLabel.text=names[Int.random(in: 0..<names.count)]
+       
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
 
