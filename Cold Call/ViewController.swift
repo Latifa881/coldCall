b20c20928fa6046b940a7fa20c11ae29febe844d
Latifa881 <l-n-a-m@hotmail.com>
2021-12-02 03:17:08 -0800

    first

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var randomNumberLabel: UILabel!
     
     let names:[String]=["Latifa","Sara","Israa","Ikram","Ragahad","Jumanah","Asrar","Lana","Munazzah","Malak"]
    
@@ -17,7 +18,18 @@ class ViewController: UIViewController {
     @IBAction func coldCallButton(_ sender: Any) {
        
         nameLabel.text=names[Int.random(in: 0..<names.count)]
-       
+        let randomNum=Int.random(in: 1...5)
+        randomNumberLabel.isHidden=false
+        randomNumberLabel.text = String(randomNum)
+        switch randomNum{
+        case 1, 2:
+            randomNumberLabel.textColor  = .red
+        case 3,4:
+            randomNumberLabel.textColor  = .orange
+        default://randomNum=5
+            randomNumberLabel.textColor  = .green
+        }
+        
         
         
     }
