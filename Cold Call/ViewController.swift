82644263df0a122be1b85a5b632da7d349e6eddb
Latifa881 <l-n-a-m@hotmail.com>
2021-12-02 00:21:58 -0800

    Initial Commit

---
+++ Cold Call/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cold Call
+//
+//  Created by administrator on 02/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
